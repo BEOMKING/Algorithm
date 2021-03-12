bc4212c33245fe4f33dc9ac21be34af9dc7fa36f
BEOMKING <qjawlsqjacks@naver.com>
2021-03-12 21:43:51 +0900

    3 / 12 solution

---
+++ 구현/기능개발.java
@@ -0,0 +1,32 @@
+package 구현;
+
+import java.util.ArrayList;
+
+public class 기능개발 {
+    public ArrayList<Integer> solution(int[] progresses, int[] speeds) {
+        int value = 0;
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        while(value != progresses.length) {
+            int day = 1;
+            while (progresses[value] + day * speeds[value] < 100) {
+                day += 1;
+            }
+            for (int i = value; i < progresses.length; i++) {
+                progresses[i] += day * speeds[i];
+            }
+
+            int count = 0;
+            for (int i = value; i < progresses.length; i++) {
+                if(progresses[i] < 100){
+                    break;
+                }
+                count += 1;
+            }
+            answer.add(count);
+            value += count;
+        }
+
+        return answer;
+    }
+}
