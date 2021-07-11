8228fd0b8c84fe0d5210d74197d96b2b4f5d52fc
BEOMKING <qjawlsqjacks@naver.com>
2021-07-11 23:17:24 +0900

    7 / 11 solution

--- DFSBFS/백준_13549_숨바꼭질3.java
+++ DFSBFS/백준_13549_숨바꼭질3.java
@@ -1,13 +1,59 @@
 package DFSBFS;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.PriorityQueue;
+import java.util.Scanner;
 
+public class 백준_13549_숨바꼭질3 {
+    static int N, K, dis[];
+    static PriorityQueue<Subin> pq;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        K = sc.nextInt();
+        pq = new PriorityQueue<>();
+        dis = new int[100001];
+        Arrays.fill(dis,100001);
+        pq.add(new Subin(N, 0));
+        dis[N] = 0;
+
+        if(N > K) System.out.println(N - K);
+        else {
+            while (!pq.isEmpty()) {
+                Subin now = pq.poll();
+                if (now.location == K) {
+                    System.out.print(now.time);
+                    break;
+                }
+                isavailable(now.location + 1, now.time + 1);
+                isavailable(now.location - 1, now.time + 1);
+                isavailable(now.location * 2, now.time);
+            }
+        }
+    }
+    static void isavailable(int location, int time){
+        if(location > 0 && location <= 100000){
+            if(dis[location] > time) {
+                dis[location] = time;
+                pq.add(new Subin(location, time));
+            }
+        }
+    }
+    static class Subin implements Comparable<Subin>{
+        int location;
+        int time;
+        public Subin(int location, int time) {
+            this.location = location;
+            this.time = time;
+        }
+        @Override
+        public int compareTo(Subin o) {
+            return time - o.time;
+        }
+    }
+}
+/*
 public class 백준_13549_숨바꼭질3 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -40,49 +86,6 @@ public class 백준_13549_숨바꼭질3 {
             if(now * 2 <= 100000 && visited[now * 2] > visited[now]){
                 visited[now * 2] = visited[now];
                 queue.offer(now * 2);
-            }
-        }
-        System.out.println(answer);
-    }
-}
-/*
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
-        st = new StringTokenizer(br.readLine());
-
-        int subin = Integer.parseInt(st.nextToken());
-        int dongsang = Integer.parseInt(st.nextToken());
-        int visited[] = new int[100001];
-        Arrays.fill(visited, -1);
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(subin);
-        visited[subin] = 0;
-        int answer = 0;
-
-        while(!queue.isEmpty()){
-            int now = queue.poll();
-            if(now == dongsang){
-                answer = visited[now];
-                break;
-            }
-            if(now * 2 <= 100000 && visited[now * 2] == -1){
-                visited[now * 2] = visited[now];
-                queue.offer(now * 2);
-            }
-            if(now - 1 >= 0 && visited[now - 1] == -1){
-                visited[now - 1] = visited[now] + 1;
-                queue.offer(now - 1);
-            }
-            if(now + 1 <= 100000 && visited[now + 1] == -1){
-                visited[now + 1] = visited[now] + 1;
-                queue.offer(now + 1);
             }
         }
         System.out.println(answer);
