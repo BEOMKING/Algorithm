5835cfd9d64d758e47bd5453482b71ba3b5f1de8
박범진 <qjawlsqjacks@naver.com>
2021-10-09 19:27:02 +0900

    10 / 9 solution

---
+++ 구현/문자열/BOJ_10173_니모를찾아서.java
@@ -0,0 +1,27 @@
+package 구현.문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class BOJ_10173_니모를찾아서 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Pattern pattern = Pattern.compile("nemo");
+        String input;
+
+        while (!(input = br.readLine()).equals("EOI")) {
+            Matcher matcher = pattern.matcher(input.toLowerCase(Locale.ROOT));
+            if(matcher.find()){
+                System.out.println("Found");
+            }else{
+                System.out.println("Missing");
+            }
+        }
+    }
+
+}
---
+++ 구현/문자열/BOJ_15881_PenPineappleApplePen.java
@@ -0,0 +1,27 @@
+package 구현.문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class BOJ_15881_PenPineappleApplePen {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String input = br.readLine();
+
+        Pattern pattern = Pattern.compile("pPAp");
+        Matcher matcher = pattern.matcher(input);
+
+        int count = 0;
+        while (matcher.find()){
+            count++;
+        }
+        System.out.println(count);
+
+    }
+
+}
