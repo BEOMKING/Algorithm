20a099e086f5cea1e388cfce164578b20786cb9e
박범진 <qjawlsqjacks@naver.com>
2022-06-20 22:51:38 +0900

    update

--- DP/BOJ_12865_평범한배낭.java
+++ DP/BOJ_12865_평범한배낭.java
@@ -6,32 +6,53 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BOJ_12865_평범한배낭 {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
-
-        st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken()); // 물건의 수
-        int K = Integer.parseInt(st.nextToken()); // 배낭 무게
-        int Weight[] = new int[N + 1];
-        int Value[] = new int[N + 1];
-        int DP[][] = new int[N + 1][K + 1];
-
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] weights = new int[N + 1];
+        int[] values = new int[N + 1];
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            Weight[i] = Integer.parseInt(st.nextToken());
-            Value[i] = Integer.parseInt(st.nextToken());
+            weights[i] = Integer.parseInt(st.nextToken());
+            values[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int n = 1; n <= N; n++) {
-            for (int k = 1; k <= K; k++) {
-                if(Weight[n] <= k){
-                    DP[n][k] = Math.max(DP[n - 1][k], DP[n - 1][k - Weight[n]] + Value[n]);
-                }else{
-                    DP[n][k] = DP[n - 1][k];
+        int[][] dp = new int[N + 1][K + 1];
+        for (int item = 1; item <= N; item++) {
+            for (int weight = 1; weight <= K; weight++) {
+                dp[item][weight] = dp[item - 1][weight];
+                if (weight - weights[item] >= 0) {
+                    dp[item][weight] = Math.max(dp[item - 1][weight], dp[item - 1][weight - weights[item]] + values[item]);
                 }
             }
         }
-        System.out.print(DP[N][K]);
+        System.out.println(dp[N][K]);
     }
+
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//        int N = Integer.parseInt(st.nextToken());
+//        int K = Integer.parseInt(st.nextToken());
+//        int[] weights = new int[N + 1];
+//        int[] values = new int[N + 1];
+//        for (int i = 1; i <= N; i++) {
+//            st = new StringTokenizer(br.readLine());
+//            weights[i] = Integer.parseInt(st.nextToken());
+//            values[i] = Integer.parseInt(st.nextToken());
+//        }
+//
+//        int[] dp = new int[K + 1];
+//        for (int item = 1; item <= N; item++) {
+//            for (int weight = K; weight >= 1; weight--) {
+//                if (weight - weights[item] >= 0) {
+//                    dp[weight] = Math.max(dp[weight], dp[weight - weights[item]] + values[item]);
+//                }
+//            }
+//        }
+//        System.out.println(dp[K]);
+//    }
 }
