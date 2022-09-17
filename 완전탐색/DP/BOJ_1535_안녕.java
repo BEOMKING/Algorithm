a56fdcc490850b989d8c6d82a713706ab0491e32
BEOMKING <qjawlsqjacks@naver.com>
2022-09-17 16:33:48 +0900

    DP, DFS

---
+++ 완전탐색/DP/BOJ_1535_안녕.java
@@ -0,0 +1,40 @@
+package 완전탐색.DP;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1535_안녕 {
+    static int N, dp[][], life[], hn[];
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        dp = new int[N + 1][100];
+        hn = new int[N + 1];
+        life = new int[N + 1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            life[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int j = 1; j <= N; j++) {
+            hn[j] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= 99; j++) {
+                if (j - life[i] >= 0) {
+                    dp[i][j] = Math.max(dp[i - 1][j],  dp[i - 1][j - life[i]] + hn[i]);
+                }else{
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        System.out.println(dp[N][99]);
+    }
+}
---
+++ 완전탐색/DP/BOJ_2193_이친수.java
@@ -0,0 +1,25 @@
+package 완전탐색.DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_2193_이친수 {
+    public static final int RANGE = 91;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        long[][] dp = new long[RANGE][2];
+        dp[1][1] = 1; dp[2][0] = 1;
+        for (int i = 3; i < RANGE; i++) {
+            dp[i][0] = dp[i - 1][1] + dp[i - 1][0];
+            dp[i][1] = dp[i - 1][0];
+        }
+        long sum = 0;
+        for (int i = 0; i < 2; i++) {
+            sum += dp[N][i];
+        }
+        System.out.println(sum);
+    }
+}
