525a0b7cf71cf240e9ce912d1676d0e6d74b6555
BEOMKING <qjawlsqjacks@naver.com>
2022-09-20 21:57:04 +0900

    DP

---
+++ 완전탐색/DP/Pro_코딩테스트공부.java
@@ -0,0 +1,49 @@
+package 완전탐색.DP;
+
+class Pro_코딩테스트공부 {
+    public int solution(int alp, int cop, int[][] problems) {
+        int maxAlp = 0;
+        int maxCop = 0;
+        for (int[] problem : problems) {
+            maxAlp = Math.max(maxAlp, problem[0]);
+            maxCop = Math.max(maxCop, problem[1]);
+        }
+
+        if (alp >= maxAlp && cop >= maxCop) {
+            return 0;
+        }
+
+        if (alp >= maxAlp) {
+            alp = maxAlp;
+        }
+        if (cop >= maxCop) {
+            cop = maxCop;
+        }
+
+        int[][] dp = new int[maxAlp + 2][maxCop + 2];
+        for (int i = alp; i < maxAlp + 1; i++) {
+            for (int j = cop; j < maxCop + 1; j++) {
+                dp[i][j] = 100000;
+            }
+        }
+
+        dp[alp][cop] = 0;
+        for (int i = alp; i < maxAlp + 1; i++) {
+            for (int j = cop; j < maxCop + 1; j++) {
+                dp[i + 1][j] = Math.min(dp[i + 1][j], dp[i][j] + 1);
+                dp[i][j + 1] = Math.min(dp[i][j + 1], dp[i][j] + 1);
+                for (int[] problem : problems) {
+                    if (i >= problem[0] && j >= problem[1]) {
+                        int nAlp = i + problem[2];
+                        int nCop = j + problem[3];
+                        if (nAlp > maxAlp) nAlp = maxAlp;
+                        if (nCop > maxCop) nCop = maxCop;
+                        dp[nAlp][nCop] = Math.min(dp[nAlp][nCop], dp[i][j] + problem[4]);
+                    }
+                }
+            }
+        }
+
+        return dp[maxAlp][maxCop];
+    }
+}
