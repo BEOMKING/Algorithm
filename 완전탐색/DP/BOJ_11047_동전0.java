7abcf77ab57de2c97d6e7b9d8e0d3c4a8475f773
BEOMKING <qjawlsqjacks@naver.com>
2022-09-26 21:35:14 +0900

    DP, Greedy - 동전 0, 회의실 배정

--- 완전탐색/DP/BOJ_11047_동전0.java
+++ 완전탐색/DP/BOJ_11047_동전0.java
@@ -1,15 +1,11 @@
-package 그리디;
+package 완전탐색.DP;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class BOJ_11047_동전0 {
-    static int N;
-    static int K;
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" ");
         int N = Integer.parseInt(input[0]);
@@ -20,14 +16,13 @@ public class BOJ_11047_동전0 {
             coins[i] = Integer.parseInt(br.readLine());
         }
 
-        int count = 0;
-        for (int i = N - 1; i >= 0; i--) {
-            if (K >= coins[i]) {
-                count += K / coins[i];
-                K %= coins[i];
+        int[] dp = new int[K + 1];
+        for (int i = 0; i < N; i++) {
+            for (int j = coins[i]; j <= K; j++) {
+                dp[j] = dp[j - coins[i]] + 1;
             }
         }
 
-        System.out.println(count);
+        System.out.println(dp[K]);
     }
 }
